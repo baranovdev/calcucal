93bbd9726f3628e66ec81da4cde6cfaf3dcd577a
VasilevichDev <82218030+VasilevichDev@users.noreply.github.com>
2021-06-13 19:47:43 +0300

    nav host victory

--- app/src/main/java/by/lebedev/calcucal/MainActivity.kt
+++ app/src/main/java/by/lebedev/calcucal/MainActivity.kt
@@ -2,16 +2,15 @@ package by.lebedev.calcucal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
     }
 }

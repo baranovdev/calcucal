d8ab98a215ea6a465524bbc481285ef3dd9cdf23
baranov-i <baranoffs.life@gmail.com>
2021-06-13 20:58:28 +0300

    Add MealType enum

---
+++ app/src/main/java/by/lebedev/calcucal/ui/main/database/entity/MealType.kt
@@ -0,0 +1,36 @@
+package by.lebedev.calcucal.ui.main.database.entity
+
+enum class MealTypeName(name:String) {
+    BEEF("Говядина"),
+    PORK("Свинина"),
+    VEAL("Телятина"),
+    MILK("Молоко"),
+    KEFIR("Кефир"),
+    YOGURT("Йогурт"),
+    PERCH("Окунь"),
+    SAURY("Сайра"),
+    SALMON("Семга"),
+    HALIBUT("Палтус"),
+    HARD_CHEESE("Твердый сыр"),
+    SOFT_CHEESE("Мягкий сыр"),
+    COTTAGE_CHEESE("Творог"),
+    MANDARIN("Мандарин"),
+    POTATO_PUREE("Пюре картофельное"),
+    POTATO_FRIED("Жареный картофель"),
+    RISE("Рис"),
+    PEARL_BARLEY("Перловка"),
+    MILLET("Пшено"),
+    CUCUMBER("Огурец"),
+    BANANA("Банан"),
+    BOILED_EGG("Вареное яйцо"),
+    TOMATO("Помидор"),
+    APPLE("Яблоко"),
+    BLACK_BREAD("Черный хлеб"),
+    WHITE_BREAD("Белый хлеб"),
+    ORANGE("Апельсин"),
+    STRAWBERRY("Клубника"),
+    PANCAKE("Блины"),
+    BORSCHT("Борщ")
+
+
+}
